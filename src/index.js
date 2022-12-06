ddbc601b3adbb263a8ff1f33fde97dc042d53cb1
phancthanhduc <phancthanhduc@gmail.com>
2022-12-06 19:50:40 +0700

    Refactor code: create routes, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ const handlebars = require('express-handlebars')
 const morgan = require('morgan')
 const app = express()
 const port = 3000
+const routes = require('./routes')
 
 //use static files
 app.use(express.static(path.join(__dirname, 'public/')))
 
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 //Http logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 //Template engine
 app.engine('hbs', handlebars.engine({extname: '.hbs'}))
@@ -17,9 +20,19 @@ app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
+// app.get('/', (req, res) => {
+//   res.render('home')
+// })
+
+// app.get('/search', (req, res) => {
+//   res.render('search')
+// })
+
+// app.post('/search', (req, res) => {
+//   console.log(req.body)
+//   res.render('search')
+// })
+routes(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const newsRouter = require('./news')
+const siteController = require('./site')
+
+function route(app) {
+    app.use('/news', newsRouter)
+    app.use('/', siteController)
+}
+
+module.exports = route
